ab0667b865e78bbbe9fba4c0f65ba9160827baa9
jiabinxu <284696433@qq.com>
2017-11-19 10:49:26 +0800

    Check for existance to avoid EXC_BAD_ACCESS #2125

--- ios/RCCViewController.m
+++ ios/RCCViewController.m
@@ -610,19 +610,24 @@ const NSInteger TRANSPARENT_NAVBAR_TAG = 78264803;
     self.navBarHairlineImageView.hidden = NO;
   }
   
- //Bug fix: in case there is a interactivePopGestureRecognizer, it prevents react-native from getting touch events on the left screen area that the gesture handles
- //overriding the delegate of the gesture prevents this from happening while keeping the gesture intact (another option was to disable it completely by demand)
- if(self.navigationController.viewControllers.count > 1){
-   if (self.navigationController != nil && self.navigationController.interactivePopGestureRecognizer != nil)
-   {
-     id <UIGestureRecognizerDelegate> interactivePopGestureRecognizer = self.navigationController.interactivePopGestureRecognizer.delegate;
-     if (interactivePopGestureRecognizer != nil && interactivePopGestureRecognizer != self)
-     {
-       self.originalInteractivePopGestureDelegate = interactivePopGestureRecognizer;
-       self.navigationController.interactivePopGestureRecognizer.delegate = self;
-     }
-   }
- }
+// deactivate on iPad to avoid crash
+if ( UI_USER_INTERFACE_IDIOM() != UIUserInterfaceIdiomPad ) {
+  //Bug fix: in case there is a interactivePopGestureRecognizer, it prevents react-native from getting touch events on the left screen area that the gesture handles
+  //overriding the delegate of the gesture prevents this from happening while keeping the gesture intact (another option was to disable it completely by demand)
+  if(
+    self.navigationController != nil &&
+    self.navigationController.viewControllers != nil &&
+    self.navigationController.viewControllers.count > 1 &&
+    self.navigationController.interactivePopGestureRecognizer != nil
+  ){
+    id <UIGestureRecognizerDelegate> interactivePopGestureRecognizer = self.navigationController.interactivePopGestureRecognizer.delegate;
+    if (interactivePopGestureRecognizer != nil && interactivePopGestureRecognizer != self)
+    {
+      self.originalInteractivePopGestureDelegate = interactivePopGestureRecognizer;
+      self.navigationController.interactivePopGestureRecognizer.delegate = self;
+    }
+  }
+}
   
   NSString *navBarCustomView = self.navigatorStyle[@"navBarCustomView"];
   if (navBarCustomView && ![self.navigationItem.titleView isKindOfClass:[RCCCustomTitleView class]]) {
